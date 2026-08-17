393ddadbff7d3307098e09443f8aae951c7cdcdf
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2026-08-17 09:40:28 +0100

    Add insert method to LinkedList for inserting nodes at specified index

--- basics/main.py
+++ basics/main.py
@@ -77,6 +77,22 @@ class LinkedList:
             temp.value=value
             return True
         return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        
+        new_node=Node(value)
+        temp=self.get(index - 1)
+        new_node.next=temp.next
+        temp.next=new_node
+        self.length+=1
+        return True
         
 my_linked_list = LinkedList(0)
 my_linked_list.append(1)
