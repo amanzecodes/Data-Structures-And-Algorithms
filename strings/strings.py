0729667801538835d767fe3e1169ac547c44c821
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2026-09-19 18:49:21 +0100

    Add initial strings.py file for basic Python string operations

---
+++ strings/strings.py
@@ -0,0 +1,7 @@
+# This is just learning the basics of python strings.
+# This is just a reference I went through the docs, but like didn't find any meaningful shii to write in here
+
+name = "Bruno "
+
+print(name * 3)
+print(name[0])
