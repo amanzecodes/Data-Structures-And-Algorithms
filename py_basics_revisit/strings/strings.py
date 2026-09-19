cc5cbb348ec0a13cf5c5ed1f9d9f93ffbd5663f0
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2026-09-19 21:01:53 +0100

    Reintroduce strings.py with basic Python string operations

---
+++ py_basics_revisit/strings/strings.py
@@ -0,0 +1,33 @@
+# This is just learning the basics of python strings.
+# This is just a reference I went through the docs, but like didn't find any meaningful shii to write in here
+
+name = "Bruno "
+
+# Implicit Concatenation
+fullname = 'Amanze' ' Jnr'
+
+job="Software Engineer"
+
+text = """
+Hey Look, it's a bug
+in the second like of the multiline string.
+"""
+
+
+print("String Multiplication: ", name * 3)
+
+print("String Indexing: ", name[0])
+
+print("Negative indexing: ", name[-3])
+
+print("Implicit Concatenation: ", fullname)
+
+print("String Slicing: ", name[1:4])
+
+print("Length of String: ", len(name))
+
+print("String Methods (UPPERCASE): ", name.upper())
+
+print("String Methods (lowercase): ", name.lower())
+
+print("Multiline String: ", text)
